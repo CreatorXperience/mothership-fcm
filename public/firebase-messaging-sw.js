1108d2c4a14f92fc18c2ae92dc2ed38ba2e1b1a8
Muhammad Habeeb <allyearmustobey@gmail.com>
2025-03-25 18:18:41 +0100

    reinitial firebase in service worker file

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,12 +1,20 @@
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
-// Get Firebase Messaging instance
+// Initialize Firebase inside the Service Worker
+firebase.initializeApp({
+    apiKey: "AIzaSyAC9ahUVhfibhFRnYg3wblneu2tZ-5h6M0",
+    authDomain: "themothershp-mvp.firebaseapp.com",
+    projectId: "themothershp-mvp",
+    storageBucket: "themothershp-mvp.firebasestorage.app",
+    messagingSenderId: "1035774205174",
+    appId: "1:1035774205174:web:f004f42355cb86a72ee0d7"
+  });
+
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log("[firebase-messaging-sw.js] Received background message", payload);
-
   const notificationTitle = payload.notification?.title || "Background Message Title";
   const notificationOptions = {
     body: payload.notification?.body || "Background Message body.",
