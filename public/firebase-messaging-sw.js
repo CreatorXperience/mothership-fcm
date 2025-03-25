3fe26658a86d1d0a10dc4a584aa96d04b3ee942b
Muhammad Habeeb <allyearmustobey@gmail.com>
2025-03-25 18:02:51 +0100

    update servicew-worker file

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,16 +1,17 @@
-import { getMessaging } from "firebase/messaging/sw";
-import { onBackgroundMessage } from "firebase/messaging/sw";
-
-const messaging = getMessaging();
-onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = payload.data.title;
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
+self.addEventListener("push", async () => {
+    const { getMessaging, onBackgroundMessage } = await import("firebase/messaging/sw");
+  
+    const messaging = getMessaging();
+    
+    onBackgroundMessage(messaging, (payload) => {
+      console.log("[firebase-messaging-sw.js] Received background message", payload);
+  
+      const notificationTitle = payload.notification?.title || "Background Message Title";
+      const notificationOptions = {
+        body: payload.notification?.body || "Background Message body.",
+        icon: "/firebase-logo.png",
+      };
+  
+      self.registration.showNotification(notificationTitle, notificationOptions);
+    });
+  });
